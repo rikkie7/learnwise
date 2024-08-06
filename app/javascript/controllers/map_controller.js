eeeb7600073857bfe06ecd7452373c535584d60c
Yannan Sun <yannansun0723@gmail.com>
2024-08-06 19:09:28 +1000

    bookings-controller done

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus"
+import mapboxgl from "mapbox-gl"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    marker: Object
+  }
+
+  connect() {
+    console.log("hello");
+    mapboxgl.accessToken = this.apiKeyValue
+
+    this.map = new mapboxgl.Map({
+      container: this.element,
+      style: "mapbox://styles/mapbox/streets-v10"
+    })
+
+    this.#addMarkerToMap()
+    this.#fitMarkerToMap()
+  }
+
+    #addMarkerToMap(){
+        const popup = new mapboxgl.Popup().setHTML(this.markerValue.info_window)
+
+        new mapboxgl.Marker()
+        .setLngLat([this.markerValue.lng, this.markerValue.lat])
+        .setPopup(popup)
+        .addTo(this.map)
+      }
+
+    #fitMarkerToMap(){
+      const bounds = new mapboxgl.LngLatBounds();
+      bounds.extend([this.markerValue.lng, this.markerValue.lat]);
+      this.map.fitBounds(bounds, { padding: 70, maxZoom: 12 })
+
+    }
+}
--- app/models/course.rb
+++ app/models/course.rb
@@ -12,5 +12,4 @@ class Course < ApplicationRecord
 
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
-
 end
