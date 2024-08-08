30cabc23ea902a9df17d5cff9d22effe5e632a5e
Lucas Siviglia <siviglialucas@gmail.com>
2024-08-08 18:59:07 +1000

    adding style to dashboard page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# puts "Destroying all users"
-# User.destroy_all
-# puts "Destroying all courses"
-# Course.destroy_all
 # puts "Destroying all sessions"
 # Session.destroy_all
+# puts "Destroying all courses"
+# Course.destroy_all
+# puts "Destroying all users"
+# User.destroy_all
+
+
 
 puts "Creating users ..."
 user1 = User.create!(first_name: "John", last_name: "Doe", email: "user1@gmail.com", password: "password", username: "John")
@@ -16,13 +18,13 @@ puts "#{User.last.username} created!"
 puts "Creating courses ..."
 
 
-Course.create!(title: "Ruby on Rails", description: "Learn Ruby on Rails", category: "technology", size: "group", price: 100, user: user1, start_date: "2024-08-01", end_date: "2024-12-01", location: "Bennelong Point, Sydney NSW 2000")
+Course.create!(title: "Ruby on Rails", description: "Learn Ruby on Rails", category: "technology", size: "group", price: 100, image_url: "https://community.theforeman.org/uploads/default/optimized/2X/8/83d09280c8809460222474ef8f7dbc94ee39abc9_2_690x362.png", user: user1, start_date: "2024-08-01", end_date: "2024-12-01", location: "Bennelong Point, Sydney NSW 2000")
 puts "#{Course.last.title} created!"
-Course.create!(title: "React", description: "Learn React", category: "technology", size: "group", price: 200, user: user2, start_date: "2024-08-01", end_date: "2024-12-01", location: "Corner Swanston Street & Flinders Street, Melbourne VIC 3000")
+Course.create!(title: "React", description: "Learn React", category: "technology", size: "group", price: 200, image_url: "https://images.unsplash.com/photo-1687603921109-46401b201195?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", user: user2, start_date: "2024-08-01", end_date: "2024-12-01", location: "Corner Swanston Street & Flinders Street, Melbourne VIC 3000")
 puts "#{Course.last.title} created!"
-Course.create!(title: "Yoga", description: "Learn Yoga", category: "sports", size: "private", price: 50, user: user1, start_date: "2024-08-01", end_date: "2024-12-01", location: "Clem Jones Promenade, South Brisbane QLD 4101")
+Course.create!(title: "Yoga", description: "Learn Yoga", category: "sports", size: "private", price: 50, image_url: "https://images.unsplash.com/photo-1506126613408-eca07ce68773?q=80&w=2202&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", user: user1, start_date: "2024-08-01", end_date: "2024-12-01", location: "Clem Jones Promenade, South Brisbane QLD 4101")
 puts "#{Course.last.title} created!"
-Course.create!(title: "Cooking", description: "Learn Cooking", category: "cooking", size: "group", price: 150, user: user3, start_date: "2024-08-01", end_date: "2024-12-01", location: "Fraser Avenue, Perth WA 6005")
+Course.create!(title: "Cooking", description: "Learn Cooking", category: "cooking", size: "group", price: 150, image_url: "https://images.unsplash.com/photo-1556910103-1c02745aae4d?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", user: user3, start_date: "2024-08-01", end_date: "2024-12-01", location: "Fraser Avenue, Perth WA 6005")
 
 puts "#{Course.last.title} created!"
 
