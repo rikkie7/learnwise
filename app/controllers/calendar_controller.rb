0f2c5ab6d93bdb4838b1fc388eede9cb00103b6e
Ruiqi Guan <rickyguan1994@hotmail.com>
2024-08-24 10:19:22 +1000

    added my calendar page and cancel booking function

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,4 +5,16 @@ class CalendarController < ApplicationController
     @bookings = Booking.all
     @events = Event.all
   end
+
+  def mycalendar
+    @all_sessions = []
+    @courses = Course.where(user:current_user)
+    @bookings = Booking.where(user:current_user)
+    @courses.each do |course|
+      @all_sessions.concat(course.sessions)
+    end
+    @bookings.each do |booking|
+      @all_sessions.concat(booking.course.sessions)
+    end
+  end
 end
