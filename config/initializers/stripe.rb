256c61a86aa99c57f10d86f7866078e74bd6f1a7
Ruiqi Guan <rickyguan1994@hotmail.com>
2024-08-29 20:20:46 +1000

    stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -6,8 +6,9 @@ Rails.configuration.stripe = {
   signing_secret:  ENV['STRIPE_WEBHOOK_SECRET_KEY']
 }
 
-Stripe.api_key = ENV['STRIPE_SECRET_KEY']
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
+Stripe.api_key = ENV['STRIPE_SECRET_KEY']
+
 StripeEvent.signing_secret = Rails.configuration.stripe[:signing_secret]
 
 StripeEvent.configure do |events|
