99bf5ca8f5b6ae0f9aa8877e1f41da97459700d3
Ruiqi Guan <rickyguan1994@hotmail.com>
2024-08-29 21:05:30 +1000

    change

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -1,7 +1,7 @@
 class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
-    order.update(state: 'paid')
+    order.update(state: 'succeeded')
     Booking.create!(user: order.user, course: order.course, status: true)
   end
 end
